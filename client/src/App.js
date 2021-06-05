0a6fc728137d67576f947a9998d3272d76d92fd9
08jhs05 <08jhs05@gmail.com>
2021-06-05 00:47:01 -0400

    create sidebar component and add it to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
 import { useEffect, useState } from "react";
 import './App.css';
 import axios from "axios";
+import Sidebar from './components/Sidebar/Sidebar';
+import Dashboard from './components/Dashboard';
 
 function App() {
 
@@ -19,20 +20,8 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {fakedata}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar />
+      <Dashboard />
     </div>
   );
 }
--- client/src/component/SideTab.js
+++
@@ -1,5 +0,0 @@
-export default function SideTab() {
-  return (
-    <div></div>
-  );
-}
