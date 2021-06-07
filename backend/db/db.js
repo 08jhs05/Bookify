403e1064133d67434434c181cf291dd1bb191b69
Imtiaz <imtiazmiah@gmail.com>
2021-06-06 20:27:32 -0400

    Seed mongo database

---
+++ backend/db/db.js
@@ -0,0 +1,190 @@
+// import testDeposit from './deposit_seeds'
+// import testExpense from './deposit_seeds'
+const testDeposit = require('./deposit_seeds')
+const testExpense = require('./expense_seeds')
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/accountDB', {useNewUrlParser: true, useUnifiedTopology: true, userFindAndModify: false});
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String
+  },
+
+  password: {
+    type: String
+  }
+});
+
+
+const depositSchema = new mongoose.Schema({
+  user_id: userSchema,
+  
+  depositDate: {
+    type: String
+  },
+
+  amount: {
+    type: Number
+  },
+
+  category: {
+    type: Array
+  },
+
+  notes: String
+});
+
+
+const expenseSchema = new mongoose.Schema({
+  user_id: userSchema,
+  
+  depositDate: {
+    type: String
+  },
+
+  amount: {
+    type: Number
+  },
+
+  category: {
+    type: Array
+  },
+
+  notes: String
+});
+
+
+/////////////////////
+const Deposit = mongoose.model("Deposit", depositSchema)
+const Expense = mongoose.model("Expense", expenseSchema)
+const User = mongoose.model("User", userSchema)
+
+// This deletes entire collection
+Deposit.deleteMany({}, function(err){
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Document has been Successfully Deleted!");
+  }
+});
+
+Expense.deleteMany({}, function(err){
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Document has been Successfully Deleted!");
+  }
+});
+
+
+const testUser = new User({
+  username: "Saitama",
+  password: "asdf"
+})
+
+
+
+Deposit.insertMany(testDeposit,
+  function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Successfully saved all the deposits");
+      }}
+)
+
+Expense.insertMany(testExpense,
+  function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Successfully saved all the expenses");
+      }}
+)
+
+
+
+
+// const test_expense = new Expense ({
+//   user_id: testUser,
+//   depositDate: "2021-01-05",
+//   amount: 50,
+//   category: ["PPE, supplies"],
+//   notes: "Buying masks for employees"
+// })
+
+
+
+
+/////////////////////////////
+
+// test_expense.save();
+
+//testUser.save();
+
+// const test_deposit = new Deposit ({
+//   user_id: testUser,
+//   depositDate: "2021-01-02",
+//   amount: 203,
+//   category: ["subsidy"],
+//   notes: "Subsidy from Federal Government"
+// })
+
+
+
+// test_deposit.save();
+
+
+
+
+// Deposit.insertMany([
+//   {
+//     user_id: testUser,
+//     depositDate: "2021-01-02",
+//     amount: 203,
+//     category: ["subsidy"],
+//     notes: "Subsidy from Federal Government"
+//   },
+//   {
+//     user_id: testUser,
+//     depositDate: "2021-01-03",
+//     amount: 400,
+//     category: ["uber"],
+//     notes: "Uber eats weekly"
+//   }
+//   ],
+//   function(err) {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         console.log("Successfully saved all the deposits");
+//       }}
+// )
+
+
+
+
+// User.updateOne({password: "asdf"}, {username: "Saitama"}, function(err){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("The document John was Successfully Updated");
+//   }
+// });
+
+
+// Deposit.deleteOne({category: ["subsidy"]}, function(err){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Document has been Successfully Deleted!");
+//   }
+// });
+
+
+
+
+
+
---
+++ backend/db/expense_seeds.js
@@ -0,0 +1,196 @@
+const testUser = { "password" : "asdf", "__v" : 0, "username" : "Saitama" }
+
+const testExpense = [
+  // SEPTEMBER
+
+  { user_id: testUser, depositDate: "2020-09-03", amount: 25000, category: ["supplies"], notes: "standard weekly supplies" },
+  { user_id: testUser, depositDate: "2020-09-03", amount: 100000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2020-09-10", amount: 500000, category: ["wages"], notes: "bi-weekly wages for employees" },
+  { user_id: testUser, depositDate: "2020-09-10", amount: 25000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2020-09-10", amount: 100000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2020-09-17", amount: 25000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2020-09-17", amount: 100000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2020-09-24", amount: 500000, category: ["wages"], notes: "bi-weekly wages for employees" },
+  { user_id: testUser, depositDate: "2020-09-24", amount: 25000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2020-09-24", amount: 100000, category: ["ingredients"], notes: "Weekly ingredients" },
+  { user_id: testUser, depositDate: "2020-09-24", amount: 80000, category: ["bills", "electric"], notes: "monthly electric bill" },
+  { user_id: testUser, depositDate: "2020-09-24", amount: 15000, category: ["bills", "gas"], notes: "monthly gas bill" },
+  { user_id: testUser, depositDate: "2020-09-24", amount: 15000, category: ["bills", "phone"], notes: "monthly phone bill" },
+  { user_id: testUser, depositDate: "2020-09-24", amount: 50000, category: ["bills", "property tax"], notes: "quarterly property tax" },
+
+
+  // OCTOBER
+
+  { user_id: testUser, depositDate: "2020-10-01", amount: 25000, category: ["supplies"], notes: "standard weekly supplies" },
+  { user_id: testUser, depositDate: "2020-10-01", amount: 100000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2020-10-08", amount: 25000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2020-10-08", amount: 100000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2020-10-15", amount: 25000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2020-10-15", amount: 100000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2020-10-22", amount: 500000, category: ["wages"], notes: "bi-weekly wages for employees" },
+  { user_id: testUser, depositDate: "2020-10-22", amount: 25000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2020-10-22", amount: 100000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2020-10-15", amount: 25000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2020-10-15", amount: 100000, category: ["ingredients"], notes: "Weekly ingredients" },
+  { user_id: testUser, depositDate: "2020-10-29", amount: 80000, category: ["bills", "electric"], notes: "monthly electric bill" },
+  { user_id: testUser, depositDate: "2020-10-29", amount: 15000, category: ["bills", "gas"], notes: "monthly gas bill" },
+  { user_id: testUser, depositDate: "2020-10-29", amount: 15000, category: ["bills", "phone"], notes: "monthly phone bill" },
+
+
+  // NOVEMBER
+
+  { user_id: testUser, depositDate: "2020-11-05", amount: 500000, category: ["wages"], notes: "bi-weekly wages for employees" },
+  { user_id: testUser, depositDate: "2020-11-05", amount: 25000, category: ["supplies"], notes: "standard weekly supplies" },
+  { user_id: testUser, depositDate: "2020-11-05", amount: 100000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2020-11-12", amount: 25000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2020-11-12", amount: 100000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2020-11-19", amount: 500000, category: ["wages"], notes: "bi-weekly wages for employees" },
+  { user_id: testUser, depositDate: "2020-11-19", amount: 25000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2020-11-19", amount: 100000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2020-11-26", amount: 25000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2020-11-26", amount: 100000, category: ["ingredients"], notes: "Weekly ingredients" },
+  { user_id: testUser, depositDate: "2020-11-26", amount: 80000, category: ["bills", "electric"], notes: "monthly electric bill" },
+  { user_id: testUser, depositDate: "2020-11-26", amount: 15000, category: ["bills", "gas"], notes: "monthly gas bill" },
+  { user_id: testUser, depositDate: "2020-11-26", amount: 15000, category: ["bills", "phone"], notes: "monthly phone bill" },
+
+
+  // DECEMBER
+
+  { user_id: testUser, depositDate: "2020-12-03", amount: 500000, category: ["wages"], notes: "bi-weekly wages for employees" },
+  { user_id: testUser, depositDate: "2020-12-03", amount: 25000, category: ["supplies"], notes: "standard weekly supplies" },
+  { user_id: testUser, depositDate: "2020-12-03", amount: 100000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2020-12-10", amount: 25000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2020-12-10", amount: 100000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2020-12-17", amount: 500000, category: ["wages"], notes: "bi-weekly wages for employees" },
+  { user_id: testUser, depositDate: "2020-12-17", amount: 25000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2020-12-17", amount: 100000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2020-12-24", amount: 25000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2020-12-24", amount: 100000, category: ["ingredients"], notes: "Weekly ingredients" },
+  { user_id: testUser, depositDate: "2020-12-24", amount: 250000, category: ["wages", "bonus"], notes: "christmas bonus" },
+
+  { user_id: testUser, depositDate: "2020-12-31", amount: 500000, category: ["wages"], notes: "bi-weekly wages for employees" },
+  { user_id: testUser, depositDate: "2020-12-31", amount: 80000, category: ["bills", "electric"], notes: "monthly electric bill" },
+  { user_id: testUser, depositDate: "2020-12-31", amount: 15000, category: ["bills", "gas"], notes: "monthly gas bill" },
+  { user_id: testUser, depositDate: "2020-12-31", amount: 15000, category: ["bills", "phone"], notes: "monthly phone bill" },
+  { user_id: testUser, depositDate: "2020-12-31", amount: 50000, category: ["bills", "property tax"], notes: "quarterly property tax" },
+
+  //JANUARY
+
+  { user_id: testUser, depositDate: "2021-01-07", amount: 30000, category: ["supplies"], notes: "standard weekly supplies" },
+  { user_id: testUser, depositDate: "2021-01-07", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2021-01-14", amount: 500000, category: ["wages"], notes: "bi-weekly wages for employees" },
+  { user_id: testUser, depositDate: "2021-01-14", amount: 30000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2021-01-14", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2021-01-21", amount: 30000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2021-01-21", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2021-01-28", amount: 500000, category: ["wages"], notes: "bi-weekly wages for employees" },
+  { user_id: testUser, depositDate: "2021-01-28", amount: 30000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2021-01-28", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+  { user_id: testUser, depositDate: "2021-01-28", amount: 80000, category: ["bills", "electric"], notes: "monthly electric bill" },
+  { user_id: testUser, depositDate: "2021-01-28", amount: 15000, category: ["bills", "gas"], notes: "monthly gas bill" },
+  { user_id: testUser, depositDate: "2021-01-28", amount: 15000, category: ["bills", "phone"], notes: "monthly phone bill" },
+
+
+  //FEBRUARY
+
+  { user_id: testUser, depositDate: "2021-02-04", amount: 30000, category: ["supplies"], notes: "standard weekly supplies" },
+  { user_id: testUser, depositDate: "2021-02-04", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2021-02-11", amount: 500000, category: ["wages"], notes: "bi-weekly wages for employees" },
+  { user_id: testUser, depositDate: "2021-02-11", amount: 30000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2021-02-11", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2021-02-18", amount: 30000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2021-02-18", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2021-02-25", amount: 500000, category: ["wages"], notes: "bi-weekly wages for employees" },
+  { user_id: testUser, depositDate: "2021-02-25", amount: 30000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2021-02-25", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+  { user_id: testUser, depositDate: "2021-02-25", amount: 80000, category: ["bills", "electric"], notes: "monthly electric bill" },
+  { user_id: testUser, depositDate: "2021-02-25", amount: 15000, category: ["bills", "gas"], notes: "monthly gas bill" },
+  { user_id: testUser, depositDate: "2021-02-25", amount: 15000, category: ["bills", "phone"], notes: "monthly phone bill" },
+
+  //MARCH
+
+  { user_id: testUser, depositDate: "2021-03-04", amount: 30000, category: ["supplies"], notes: "standard weekly supplies" },
+  { user_id: testUser, depositDate: "2021-03-04", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2021-03-11", amount: 500000, category: ["wages"], notes: "bi-weekly wages for employees" },
+  { user_id: testUser, depositDate: "2021-03-11", amount: 30000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2021-03-11", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2021-03-18", amount: 30000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2021-03-18", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2021-03-25", amount: 500000, category: ["wages"], notes: "bi-weekly wages for employees" },
+  { user_id: testUser, depositDate: "2021-03-25", amount: 30000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2021-03-25", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+  { user_id: testUser, depositDate: "2021-03-25", amount: 80000, category: ["bills", "electric"], notes: "monthly electric bill" },
+  { user_id: testUser, depositDate: "2021-03-25", amount: 15000, category: ["bills", "gas"], notes: "monthly gas bill" },
+  { user_id: testUser, depositDate: "2021-03-25", amount: 15000, category: ["bills", "phone"], notes: "monthly phone bill" },
+  { user_id: testUser, depositDate: "2020-03-25", amount: 50000, category: ["bills", "property tax"], notes: "quarterly property tax" },
+
+  //APRIL
+
+  { user_id: testUser, depositDate: "2021-04-01", amount: 30000, category: ["supplies"], notes: "standard weekly supplies" },
+  { user_id: testUser, depositDate: "2021-04-01", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2021-04-08", amount: 500000, category: ["wages"], notes: "bi-weekly wages for employees" },
+  { user_id: testUser, depositDate: "2021-04-08", amount: 30000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2021-04-08", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2021-04-15", amount: 30000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2021-04-15", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2021-04-22", amount: 500000, category: ["wages"], notes: "bi-weekly wages for employees" },
+  { user_id: testUser, depositDate: "2021-04-22", amount: 30000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2021-04-22", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+  { user_id: testUser, depositDate: "2021-04-22", amount: 30000, category: ["supplies"], notes: "Weekly supplies" },
+  { user_id: testUser, depositDate: "2021-04-22", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+  { user_id: testUser, depositDate: "2021-04-29", amount: 80000, category: ["bills", "electric"], notes: "monthly electric bill" },
+  { user_id: testUser, depositDate: "2021-04-29", amount: 15000, category: ["bills", "gas"], notes: "monthly gas bill" },
+  { user_id: testUser, depositDate: "2021-04-29", amount: 15000, category: ["bills", "phone"], notes: "monthly phone bill" },
+  { user_id: testUser, depositDate: "2020-04-29", amount: 110000, category: ["repair"], notes: "repair broken fridge" },
+
+
+    //MAY
+    { user_id: testUser, depositDate: "2021-05-06", amount: 500000, category: ["wages"], notes: "bi-weekly wages for employees" },
+    { user_id: testUser, depositDate: "2021-05-06", amount: 30000, category: ["supplies"], notes: "standard weekly supplies" },
+    { user_id: testUser, depositDate: "2021-05-06", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+  
+    { user_id: testUser, depositDate: "2021-05-13", amount: 30000, category: ["supplies"], notes: "Weekly supplies" },
+    { user_id: testUser, depositDate: "2021-05-13", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+
+    { user_id: testUser, depositDate: "2021-05-20", amount: 500000, category: ["wages"], notes: "bi-weekly wages for employees" },
+    { user_id: testUser, depositDate: "2021-05-20", amount: 30000, category: ["supplies"], notes: "Weekly supplies" },
+    { user_id: testUser, depositDate: "2021-05-20", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+  
+    { user_id: testUser, depositDate: "2021-05-27", amount: 30000, category: ["supplies"], notes: "Weekly supplies" },
+    { user_id: testUser, depositDate: "2021-05-27", amount: 150000, category: ["ingredients"], notes: "Weekly ingredients" },
+    { user_id: testUser, depositDate: "2021-05-27", amount: 30000, category: ["supplies"], notes: "Weekly supplies" },
+    { user_id: testUser, depositDate: "2021-05-27", amount: 80000, category: ["bills", "electric"], notes: "monthly electric bill" },
+    { user_id: testUser, depositDate: "2021-05-27", amount: 15000, category: ["bills", "gas"], notes: "monthly gas bill" },
+    { user_id: testUser, depositDate: "2021-05-27", amount: 15000, category: ["bills", "phone"], notes: "monthly phone bill" }
+
+
+];
+
+
+module.exports = testExpense;
