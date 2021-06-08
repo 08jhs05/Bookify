1904746eb436647d95dc3e3cdb5c4ff5e30bbfee
08jhs05 <08jhs05@gmail.com>
2021-06-08 13:29:26 -0400

    lint code

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -22,7 +22,7 @@ export default function Dashboard(props) {
 
     const promises = [axios.get('/api/incomes', { params: { queryDate: state.queryDate } }), axios.get('/api/expenses', { params: { queryDate: state.queryDate } })];
     Promise.all(promises).then( (res) => {
-      setState({ ...state, data: { incomes: res[0].data, expenses: res[1].data}});
+      setState({ ...state, data: { incomes: res[0].data, expenses: res[1].data }});
       console.log(res[0].data);
       console.log(res[1].data);
     });
