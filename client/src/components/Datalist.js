088e5e1b0a4091ad46d5b0a575003148888b819e
Imtiaz <imtiazmiah@gmail.com>
2021-06-11 15:39:41 -0400

    Last before merge

--- client/src/components/Datalist.js
+++ client/src/components/Datalist.js
@@ -11,29 +11,30 @@ export default function Datalist(props) {
   return (
     <section className="dataList">
       <div>
-        { Array.isArray(props.data) && props.data.map(elem => (
+        {Array.isArray(props.data) && props.data.map(elem => (
           <div className="dataRow" key={elem._id}>
-            {elem.notes}. &nbsp;&nbsp; Category: {elem.category.length > 1 ? `${elem.category[0]}-${elem.category[1]}` : elem.category}, &nbsp;&nbsp; amount: {elem.amount}, &nbsp;&nbsp; time: {elem.depositDate}
-            {props.type === "expense" ? <ExpenseEditForm data={elem} reloadPage={props.reloadPage}/> : <IncomeEditForm data={elem} reloadPage={props.reloadPage}/>}
+            {elem.notes}. &nbsp;&nbsp; Category: {elem.category.length > 1 ? `${elem.category[0]}-${elem.category[1]}` : elem.category}, &nbsp;&nbsp; amount: {elem.amount}, &nbsp;&nbsp; date: {elem.depositDate.slice(0, 10)}
+            {props.type === "expense" ? <ExpenseEditForm data={elem} reloadPage={props.reloadPage} /> : <IncomeEditForm data={elem} reloadPage={props.reloadPage} />}
             <Button variant="outlined"
-            color="secondary"
-            value={elem._id}
-            onClick={() => {
-              currentID = elem._id;
-              setOpenDeleteConfirm(true)}}>DELETE</Button>
+              color="secondary"
+              value={elem._id}
+              onClick={() => {
+                currentID = elem._id;
+                setOpenDeleteConfirm(true)
+              }}>DELETE</Button>
           </div>
         ))}
-        <Dialog open={openDeleteConfirm} onClose={() => {setOpenDeleteConfirm(false)} } aria-labelledby="delete-dialog-title">
+        <Dialog open={openDeleteConfirm} onClose={() => { setOpenDeleteConfirm(false) }} aria-labelledby="delete-dialog-title">
           <DialogContent>Are you sure you want to delete?
             <DialogActions>
               <Button onClick={() => {
                 props.deleteBtnOnClick(currentID);
                 setOpenDeleteConfirm(false);
-                }}
+              }}
                 color="secondary">
                 Yes
               </Button>
-              <Button onClick={() => {setOpenDeleteConfirm(false)}} color="primary">
+              <Button onClick={() => { setOpenDeleteConfirm(false) }} color="primary">
                 No
               </Button>
             </DialogActions>
