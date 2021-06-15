241b5294a02dd1afa4439d2619b868bcaa89839a
08jhs05 <08jhs05@gmail.com>
2021-06-15 11:34:13 -0400

    visual update for signIn page

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -21,25 +21,27 @@ import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    backgroundColor: 'whitesmoke',
+    backgroundColor: 'rgb(83, 140, 223)',
     borderRadius: '20px 0 0 20px',
     height: '100vh',
     width: '100%',
     margin: 0,
   },
   paper: {
-    marginTop: theme.spacing(20),
+    width:'25vw',
+    zIndex: '999',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: theme.spacing(6)
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '30%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
+    width: '100%', // Fix IE 11 issue.
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
@@ -90,68 +92,91 @@ export default function SignIn(props) {
 
   return (
     <section className={classes.container}>
-      <CssBaseline />
-      <div className={classes.paper}>
+      <div style={{display:'flex', alignItems:'center', justifyContent:'center', width: '100%', height: '90%'}}>
+        <div style={{display:'flex', flexDirection:'column', alignItems:'center', marginRight:'120px', zIndex:'999'}}>
+          <div style={{fontSize:'42px', color:'whitesmoke', display:'flex', flexDirection:'column', alignItems:'center', marginBottom:'10px'}}>
+            <div>Record your receipts,</div>
+            <div>smartly.</div>
+          </div>
           <img
-            className="logo"
-            src="/icons/coloredLogo.png"
-            alt="logo icon"
-          />
-        <div className="signin_title">
-          Sign in
+          src="/illustration.png"
+          alt="Illustration"
+          style={{width:'50vh', height:'50vh'}}/>
         </div>
-        <form className={classes.form}>
-          {isError && <div style={{color:'red'}}>{isError}</div>}
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            value={formValues.email}
-            onChange={handleChange}
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            value={formValues.password}
-            onChange={handleChange}
-            autoComplete="current-password"
-          />
-            <Button
-              type="submit"
+          <div className={classes.paper}>
+            <div style={{display:'flex', alignItems:'center', marginBottom:'10px'}}>
+              <img
+                style={{width:'90px', height:'90px'}}
+                src="/icons/noBGLogo.png"
+                alt="logo icon"
+              />
+              <div style={{fontSize:'38px', color:'white', marginTop:'6px'}}>Bookify</div>
+            </div>
+          <form className={classes.form}>
+            {isError && <div style={{color:'red'}}>{isError}</div>}
+            <TextField
+            style={{
+              backgroundColor: "white", borderRadius:'10px'
+              }}
+              variant="filled"
+              margin="normal"
+              required
               fullWidth
-              variant="contained"
-              color="primary"
-              // className={classes.submit}
-              onClick={ validateUser }
-            >
-              Sign In
-            </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              value={formValues.email}
+              onChange={handleChange}
+              autoFocus
+              back
+            />
+            <TextField
+            style={{
+              backgroundColor: "white", borderRadius:'10px'
+              }}
+              variant="filled"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              value={formValues.password}
+              onChange={handleChange}
+              autoComplete="current-password"
+            />
+              <Button
+              style={{marginTop:'10px', backgroundColor:'#303F9F', borderRadius:'10px'}}
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                // className={classes.submit}
+                onClick={ validateUser }
+              >
+                Sign In
+              </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  <div style={{color:'white', marginTop:'10px'}}>Forgot password?</div>
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  <div style={{color:'white', marginTop:'10px'}}>Sign Up</div>
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
+          </form>
+        </div>
       </div>
+      <img
+      src="/bg.png"
+      alt="background photo"
+      style={{position:'fixed', bottom:'0'}}/>
     </section>
   );
 }
