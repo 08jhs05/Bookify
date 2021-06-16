53f49ca50a3d18a6c353149dd41202666a553bdf
Imtiaz <imtiazmiah@gmail.com>
2021-06-16 10:54:51 -0400

    comments for skeleton of dta grid filtering

--- client/src/components/Datalist.js
+++ client/src/components/Datalist.js
@@ -67,3 +67,33 @@ export default function Datalist(props) {
   </Paper>
   );
 }
+
+
+
+/////////////////
+
+
+// import * as React from 'react';
+// import { DataGrid, GridToolbar } from '@material-ui/data-grid';
+// import { useDemoData } from '@material-ui/x-grid-data-generator';
+
+// export default function BasicFilteringGrid() {
+//   const { data } = useDemoData({
+//     dataSet: 'Commodity',
+//     rowLength: 100,
+//     maxColumns: 6,
+//   });
+
+//   return (
+//     <div style={{ height: 400, width: '100%' }}>
+//       <DataGrid
+//         {...data}
+//         components={{
+//           Toolbar: GridToolbar,
+//         }}
+//         filterModel={{
+//           items: [
+//             { columnField: 'commodity', operatorValue: 'contains', value: 'rice' },
+//           ],
+//         }}
+//       />
